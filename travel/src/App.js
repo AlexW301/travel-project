f27d3c1c36432be05453eedc269c3c42fef3c342
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-09-28 21:32:32 -0400

    update

--- travel/src/App.js
+++ travel/src/App.js
@@ -9,8 +9,15 @@ function App() {
   return (
     <div className="App">
       <div className="map-area">
-        <h1 className="heading">Find Us on The Map!</h1>
+        <h1 className="heading">Click A Location On The Map!</h1>
         <USAMap className="USA-map" onClick={mapHandler} />
+        <div className="legend-area">
+            <ul className="legend">
+                <li className="legend-item"><div className="legend-color"></div><p className="legend-text">Visited</p></li>
+                <li className="legend-item"><div className="legend-color"></div><p className="legend-text">Not Visited</p></li>
+                <li className="legend-item"><div className="legend-color"></div><p className="legend-text">Current Location</p></li>
+            </ul>
+        </div>
       </div>
       <div className="temp-space" id="test"></div>
     </div>
