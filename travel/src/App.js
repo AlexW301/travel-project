0b4d7eea91a91138bf0fb86f88ed1f70a712ad63
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-09-30 08:39:36 -0400

    update

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./App.css";
 import USAMap from "react-usa-map";
-import adamAndRachelPic from './photos/adam-and-rachel-temp.jpeg'
+import profileTemp from './photos/profile-temp.jfif'
 
 function App() {
   let mapHandler = (event) => {
@@ -18,14 +18,19 @@ function App() {
           </ul>
         </header>
         <div className="intro-section-top">
-            <img src={adamAndRachelPic} className="hero-image" alt="#"/>
             <div className="hero-text">
-              <h1 className="hero-header">A Little Bit About Us</h1>
-              <p className="hero-sub-header">Our names are Adam and Rachel and we are traveling the countryin our camper truck. Our goal is to travel across the country and see as much as possible.</p>
+              <h1 className="hero-header">LIFE IS AN ADVENTURE</h1>
+              <p className="hero-sub-header">Scroll below to see ours</p>
             </div>
         </div>
         <div className="intro-section-bottom">
-          <p className="see-our-journey-text">See Our Journey</p>
+        <div className="about-us-img">
+          <img src={profileTemp} alt="#" className="profile-img"/>
+        </div>
+          <div className="about-us-text">
+          <p className="about-us-header">A Little Bit About Us!</p>
+          <p className="about-us-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
         </div>
       </div>
 
